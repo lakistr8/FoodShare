83eb0df6adb672e8f28892af75309037d88530f0
Lazar Andonov <lakiando@outlook.com>
2018-01-24 10:54:06 +0100

    Created Login and base view controller class

---
+++ FoodShare/Base/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  FoodShare
+//
+//  Created by Lazar Andonov on 1/24/18.
+//  Copyright © 2018 Lazar Andonov. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    let clintID = "4YCWUAVMPUF1MOZEORUOFDSPFADBXL5JB0NISIJEKD3W2NKI"
+    let sicretSecret = "OP4JFDTMOTMNE1T4WZ5VLFERC2M23QDMHXP5Z4101WLTEE3V"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
