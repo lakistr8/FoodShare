7c2e05702d1e380bf628a75b0642de21af2ba593
Lazar Andonov <lakiando@outlook.com>
2018-01-25 14:03:24 +0100

    Added user location when started app app

--- FoodShare/Base/BaseViewController.swift
+++ FoodShare/Base/BaseViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation
 
-class BaseViewController: UIViewController, BaseViewControllerProtocol {
+class BaseViewController: UIViewController, BaseViewControllerProtocol, CLLocationManagerDelegate {
     
     let clientID = "4YCWUAVMPUF1MOZEORUOFDSPFADBXL5JB0NISIJEKD3W2NKI"
     let sicretSecret = "OP4JFDTMOTMNE1T4WZ5VLFERC2M23QDMHXP5Z4101WLTEE3V"
     var baseArr : [BaseData] = []
+    let locationManager = CLLocationManager()
+    var userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        userDefaults.set(location.coordinate.latitude, forKey: "lat")
+        userDefaults.set(location.coordinate.longitude, forKey: "lng")
     }
     
     func initialize(component:String, data:[BaseData]) {
@@ -29,15 +41,15 @@ class BaseViewController: UIViewController, BaseViewControllerProtocol {
     }
     
     func fetch(using query:String, and component:String) {
-        let url = "https://api.foursquare.com/v2/venues/search?client_id=\(clientID)&client_secret=\(sicretSecret)&ll=44.8193266,20.4619653&query=\(query)&v=20180124"
+        let url = "https://api.foursquare.com/v2/venues/search?client_id=\(clientID)&client_secret=\(sicretSecret)&ll=\(userDefaults.object(forKey: "lat") ?? ""),\(userDefaults.object(forKey: "lng") ?? "")&query=\(query)&v=20180124"
         let fullUrl = URL(string:url)
         Alamofire.request(fullUrl!).responseJSON { response in
             if let json = response.result.value {
                 let jSON = JSON(json)["response"]["venues"].array!
                 for item in jSON {
                     self.baseArr.append(BaseData(data: item))
-                    self.initialize(component: component, data: self.baseArr)
                 }
+                self.initialize(component: component, data: self.baseArr)
             }
         }.resume()
     }
