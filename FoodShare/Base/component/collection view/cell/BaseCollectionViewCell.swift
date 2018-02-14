b2e85d096a9c6df9b9024c6221d180e12d704b68
Lazar Andonov <lakiando@outlook.com>
2018-02-14 14:11:54 +0100

    Display at map user location and pin for any venues
    This code must be write on better way!

--- FoodShare/Base/component/collection view/cell/BaseCollectionViewCell.swift
+++ FoodShare/Base/component/collection view/cell/BaseCollectionViewCell.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import SwiftyJSON
+import CoreLocation
+import MapKit
 
-class BaseCollectionViewCell: UICollectionViewCell {
+class BaseCollectionViewCell: UICollectionViewCell, CLLocationManagerDelegate, MKMapViewDelegate {
     
     let screen = UIWindow(frame: UIScreen.main.bounds)
     var id: Int?
@@ -49,6 +51,18 @@ class BaseCollectionViewCell: UICollectionViewCell {
         detail.stateLbl.text = self.stateLbl.text
         detail.townLbl.text = self.cityLbl.text
         detail.frame = self.screen.frame
+        detail.map.centerCoordinate.latitude = self.lat!
+        detail.map.centerCoordinate.longitude = self.lng!
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(self.lat!, self.lng!)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        self.detail.map.setRegion(region, animated: true)
+        self.detail.map.showsUserLocation = true
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: self.lat!, longitude: self.lng!)
+        self.detail.map.addAnnotation(annotation)
         self.screen.addSubview(detail)
         self.closeBtn.addTarget(self, action: #selector(close), for: .touchUpInside)
         self.closeBtn.setTitle("X", for: .normal)
